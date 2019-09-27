0f62f7c4aa52f3b015a9f9fc853adb18fd7334f3
Konstantin Sofiiuk <k.sofiiuk@samsung.com>
2019-09-27 12:06:52 +0300

    Removed unnecessary AUC metric and sklearn dependency

--- adaptis/model/metrics.py
+++ adaptis/model/metrics.py
@@ -1,6 +1,5 @@
 import mxnet as mx
 import numpy as np
-import sklearn.metrics
 
 
 class TrainMetric(object):
@@ -82,46 +81,6 @@ class AdaptiveIoU(TrainMetric):
         return self._iou_thresh
 
 
-class AUC(TrainMetric):
-    def __init__(self, beta=0.9, ignore_label=-1, from_logits=True,
-                 pred_output='instances', gt_output='instances'):
-        super().__init__(pred_outputs=(pred_output,), gt_outputs=(gt_output,))
-        self._ignore_label = ignore_label
-        self._from_logits = from_logits
-        self._beta = beta
-        self._ema_auc = 0.0
-        self._epoch_auc_sum = 0.0
-        self._epoch_batch_count = 0
-
-    def update(self, pred, gt):
-        gt_mask = gt > 0
-        if self._from_logits:
-            pred = mx.nd.sigmoid(pred)
-
-        gt_mask_area = mx.nd.sum(gt_mask, axis=0, exclude=True).asnumpy()
-        if np.all(gt_mask_area == 0):
-            return
-
-        ignore_mask = gt == self._ignore_label
-        auc = _compute_auc(pred, gt_mask, ignore_mask).mean()
-        self._ema_auc = self._beta * self._ema_auc + (1 - self._beta) * auc
-        self._epoch_auc_sum += auc
-        self._epoch_batch_count += 1
-
-    def get_epoch_value(self):
-        if self._epoch_batch_count > 0:
-            return self._epoch_auc_sum / self._epoch_batch_count
-        else:
-            return 0.0
-
-    def reset_epoch_stats(self):
-        self._epoch_auc_sum = 0.0
-        self._epoch_batch_count = 0
-
-    def log_states(self, sw, tag_prefix, global_step):
-        sw.add_scalar(tag=tag_prefix + '_ema', value=self._ema_auc, global_step=global_step)
-
-
 def _compute_iou(pred_mask, gt_mask, ignore_mask, keep_ignore=False):
     pred_mask = mx.nd.where(ignore_mask, mx.nd.zeros_like(pred_mask), pred_mask)
     union = mx.nd.mean(mx.nd.logical_or(pred_mask, gt_mask), axis=0, exclude=True).asnumpy()
@@ -136,20 +95,3 @@ def _compute_iou(pred_mask, gt_mask, ignore_mask, keep_ignore=False):
         result[nonzero] = iou
         return result
 
-
-def _compute_auc(pred, gt_mask, ignore_mask):
-    pred = mx.nd.where(ignore_mask, mx.nd.zeros_like(pred), pred).asnumpy()
-    gt_mask = gt_mask.asnumpy()
-
-    samples_auc = []
-    for i in range(pred.shape[0]):
-        sample_pred = pred[i]
-        sample_gt = gt_mask[i]
-        if sample_gt.sum() == 0:
-            continue
-
-        precision, recall, _ = sklearn.metrics.precision_recall_curve(sample_gt.flatten(), sample_pred.flatten())
-        auc = sklearn.metrics.auc(recall, precision)
-        samples_auc.append(auc)
-
-    return np.array(samples_auc)
