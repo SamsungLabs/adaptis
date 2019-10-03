d6192493d86f0800618b9b4fdf755084dfc0c2ee
Konstantin Sofiiuk <k.sofiiuk@samsung.com>
2019-10-03 16:27:19 +0300

    Added new ToyV2 dataset.

--- adaptis/utils/vis.py
+++ adaptis/utils/vis.py
@@ -3,6 +3,24 @@ import numpy as np
 import cv2
 
 
+def visualize_instances(imask, bg_color=255,
+                        boundaries_color=None, boundaries_width=1, boundaries_alpha=0.8):
+    num_objects = imask.max() + 1
+    palette = get_palette(num_objects)
+    if bg_color is not None:
+        palette[0] = bg_color
+
+    result = palette[imask].astype(np.uint8)
+    if boundaries_color is not None:
+        boundaries_mask = get_boundaries(imask, boundaries_width=boundaries_width)
+        tresult = result.astype(np.float32)
+        tresult[boundaries_mask] = boundaries_color
+        tresult = tresult * boundaries_alpha + (1 - boundaries_alpha) * result
+        result = tresult.astype(np.uint8)
+
+    return result
+
+
 @lru_cache(maxsize=16)
 def get_palette(num_cls):
     palette = np.zeros(3 * num_cls, dtype=np.int32)
@@ -19,15 +37,6 @@ def get_palette(num_cls):
             lab >>= 3
 
     return palette.reshape((-1, 3))
-
-
-def visualize_instances(imask, bg_color=255):
-    num_objects = imask.max() + 1
-    palette = get_palette(num_objects)
-    if bg_color is not None:
-        palette[0] = bg_color
-
-    return palette[imask].astype(np.uint8)
 
 
 def visualize_mask(mask, num_cls):
@@ -74,3 +83,19 @@ def blend_mask(image, mask, alpha=0.6):
     imap = draw_instance_map(mask)
     result = (image * (1 - alpha) + alpha * imap).astype(np.uint8)
     return result
+
+
+def get_boundaries(instances_masks, boundaries_width=1):
+    boundaries = np.zeros((instances_masks.shape[0], instances_masks.shape[1]), dtype=np.bool)
+
+    for obj_id in np.unique(instances_masks.flatten()):
+        if obj_id == 0:
+            continue
+
+        obj_mask = instances_masks == obj_id
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        inner_mask = cv2.erode(obj_mask.astype(np.uint8), kernel, iterations=boundaries_width).astype(np.bool)
+
+        obj_boundary = np.logical_xor(obj_mask, np.logical_and(inner_mask, obj_mask))
+        boundaries = np.logical_or(boundaries, obj_boundary)
+    return boundaries
